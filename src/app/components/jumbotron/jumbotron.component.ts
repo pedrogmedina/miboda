eba6519940360ff60dcda66248a38378fcbf1f4a
DESKTOP-7C6V9IE\Pedro <perdy_184@outlook.es>
2024-04-30 21:41:47 +0200

    april version

--- src/app/components/jumbotron/jumbotron.component.ts
+++ src/app/components/jumbotron/jumbotron.component.ts
@@ -1,9 +1,22 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-jumbotron',
   templateUrl: './jumbotron.component.html',
-  styleUrls: ['./jumbotron.component.scss']
+  styleUrls: ['./jumbotron.component.scss'],
+  animations: [
+    trigger( 'visibleHidden', [
+      state(
+        'visible',
+        style({
+          transform: 'translateX(100%)scale(1)',
+          opacity: 1,
+        }),
+      ),
+      transition('* => visible',animate('230ms ease-in-out')),
+    ]),
+  ]
 })
 export class JumbotronComponent implements OnInit {
 
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -5,12 +5,14 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { PlanningComponent } from 'src/app/components/planning/planning.component';
 import { JumbotronComponent } from 'src/app/components/jumbotron/jumbotron.component';
+import { CardsComponent } from 'src/app/components/cards/cards.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
     PlanningComponent,
     JumbotronComponent,
+    CardsComponent
   ],
   imports: [
     CommonModule,
