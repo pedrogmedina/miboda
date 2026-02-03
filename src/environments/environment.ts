afae102d4cedee3e664673790ac917051effb0fb
pedrogmedina <58294231+pedrogmedina@users.noreply.github.com>
2026-02-03 14:17:02 +0100

    Remove Firebase credentials from environment file
    Cleared sensitive Firebase configuration values.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,12 +2,12 @@ export const environment = {
   production: true,
   firebase: {
     projectId: 'bodaisabelpedro-6df33',
-    appId: '1:349386229158:web:a9023d9cec4ec7495d7b67',
-    storageBucket: 'bodaisabelpedro-6df33.appspot.com',
+    appId: '',
+    storageBucket: '',
     locationId: 'europe-west',
-    apiKey: 'AIzaSyD5mI_9W0CxYPcPhY-f4EosPgZrdXYlmqY',
-    authDomain: 'bodaisabelpedro-6df33.firebaseapp.com',
-    messagingSenderId: '349386229158',
+    apiKey: '',
+    authDomain: '',
+    messagingSenderId: '',
   },
   
 };
